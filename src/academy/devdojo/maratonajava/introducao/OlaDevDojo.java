a242c0b3969266cbc3d8065543cf6a4c35f500f9
Joao Lucas Silva <joaolucassalvatore@gmail.com>
2022-01-05 21:25:55 -0300

    comments

--- src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,13 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ * This is a <b>javadoc comment<b/>
+ */
 public class OlaDevDojo {
+    // This is a line comment
+    /*
+        This is a multiple line comment
+    */
     public static void main(String[] args) {
         System.out.println("KA-ME-HA-ME-HA");
     }
