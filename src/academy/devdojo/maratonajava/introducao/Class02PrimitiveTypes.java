16d2d119e4c4e98ad96c860b81dc149c763cf437
Joao Lucas Silva <joaolucassalvatore@gmail.com>
2022-01-05 22:01:51 -0300

    Primitive Types part II, declaration and size in memory

--- src/academy/devdojo/maratonajava/introducao/Class02PrimitiveTypes.java
+++ src/academy/devdojo/maratonajava/introducao/Class02PrimitiveTypes.java
@@ -2,8 +2,16 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Class02PrimitiveTypes {
     public static void main(String[] args) {
-        // int, double, float, char, byte, short, long, boolean
+        // byte, short, int, long, float, double, boolean, char
+        byte ageByte = 65;
+        short ageShort = 20;
         int age = 20;
+        long bigNumber = 1000000;
+        float salaryFloat = 15000;
+        double salaryDouble = 10000;
+        boolean isTrue = true;
+        boolean isFalse = false;
+        char charactere = 'M';
         System.out.println("We are " + age + " years old.");
     }
 }
